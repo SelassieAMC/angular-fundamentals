ea6bd85348b23858647736504597c0711f2ba991
Andres Mauricio Clavijo Mejia <aclavijo@belatrix.com>
2019-12-20 16:50:37 -0500

    Creating first databound Component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app',
-  template: `
-      <h2>Hello world</h2>
-      <img src="/assets/images/basic-shield.png"/>
-  `
+  selector: 'app-events',
+  template: '<app-events-list></app-events-list>'
 })
+
 export class EventsAppComponent {
   title = 'ng-fundamentals';
 }
